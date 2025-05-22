bc2b76261bdedc1704c372b8f2b527ca3ad46733
JOÃO CARVALHO <dev.joaocarvalho@gmail.com>
2025-05-22 17:08:19 -0300

    Update inventory.py

--- package/inventory.py
+++ package/inventory.py
@@ -1,7 +1,15 @@
-
 class Inventario:
     def __init__(self):
         self._itens = {"Comida": 5, "Brinquedos": 3}
 
     def usar_comida(self):
         if self._itens["Comida"] > 0:
+            self._itens["Comida"] -= 1
+            return True
+        return False
+
+    def usar_brinquedo(self):
+        if self._itens["Brinquedos"] > 0:
+            self._itens["Brinquedos"] -= 1
+            return True
+        return False
