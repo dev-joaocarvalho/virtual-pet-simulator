9182816b848f518011b96d8c6b098f56ee7b693d
JOÃO CARVALHO <dev.joaocarvalho@gmail.com>
2025-05-22 17:05:12 -0300

    Update database.py

--- package/database.py
+++ package/database.py
@@ -1,1 +1,16 @@
 
+import pickle
+
+class BancoDeDados:
+    @staticmethod
+    def salvar_pet(pet, filename="pet_save.pkl"):
+        with open(filename, 'wb') as file:
+            pickle.dump(pet, file)
+
+    @staticmethod
+    def carregar_pet(filename="pet_save.pkl"):
+        try:
+            with open(filename, 'rb') as file:
+                return pickle.load(file)
+        except FileNotFoundError:
+            return None
